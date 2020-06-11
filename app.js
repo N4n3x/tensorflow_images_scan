a2e7ec6842883a0420db94dbecabab62288fc88d
Christophe COUERBE <christophe.couerbe@viacesi.fr>
2020-06-11 15:50:27 +0200

    maj

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const Image = require('./models/imageModel');
 
 // const tf = require('@tensorflow/tfjs-node');
 // const tf = require('@tensorflow/tfjs-node-gpu');
 
+// https://mongoosejs.com/docs/
 mongoose.connect('mongodb://cesi:cesidevops2019@194.9.172.68:27017/projet2?authSource=admin', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -17,10 +19,11 @@ const db = mongoose.connection;
 db.on('error', () => {
   console.error('connection error: ');
 });
+
 db.once('open', () => {
   console.log('connecté');
 });
-// https://mongoosejs.com/docs/
+
 const app = express();
 
 // view engine setup
@@ -29,20 +32,20 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static('public'));
 
-var mainRoutes = require('./routes/main');
+var mainRoutes = require('./routes/router');
 app.use(mainRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
